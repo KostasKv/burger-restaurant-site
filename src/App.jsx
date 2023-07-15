b358d6fd50cc9c005c4e5a5a89cc190fcdda81b3
KostasKv <kostas.kvietinskas@gmail.com>
2023-07-15 13:37:28 +0100

    Better responsiveness first section img+paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <PrimaryHeader />
       <section className="min-h-screen bg-primary flex justify-center">
-        <div className="relative w-full max-w-[1800px] isolate flex flex-col min-h-screen px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem]">
+        <div className="relative w-full px-[clamp(1.25rem,10vw,5rem)] md:max-w-[1800px] isolate flex flex-col min-h-screen sm:px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem] sm:items-start">
           <h1 className="relative text-[clamp(1.5rem,1rem+6.4vw,6.875rem)] uppercase leading-none ">
             New York's <br /> favorite{" "}
             <span className="relative w-[1.08em] h-0 bg-purple-500 hidden sm:inline-block">
@@ -19,14 +19,14 @@ function App() {
             </span>
             <br /> burger
           </h1>
-          <p className="mt-3 text-lg sm:text-2xl max-w-[38rem] sm:mt-[1.19rem]">
+          <p className="mt-3 text-lg sm:text-2xl max-w-[clamp(24rem,10rem+45vw,38rem)] sm:mt-[1.19rem] bg-inherit bg-clip-text">
             In a world so painfully serious, yet so ridiculous, you knew it was
             only a matter of time before plant-based steak became a thing.
           </p>
-          <button className="mt-auto w-full h-[3.375rem] max-w-sm text-xl bg-black text-white uppercase sm:max-w-[16.875rem] sm:text-2xl sm:mt-12 hover:bg-accent duration-75 hover:text-black">
+          <button className="mt-auto w-full h-[3.375rem] max-w-[20rem] text-xl bg-black text-white uppercase sm:max-w-[16.875rem] sm:text-2xl sm:mt-12 hover:bg-accent duration-75 hover:text-black">
             Browse Menu
           </button>
-          <span className="absolute right-[clamp(1.5rem,1rem+3.3vw,4rem)] top-[clamp(21%,50%-15vw,40%)] w-[clamp(16rem,5rem+29.18vw,31.26rem)] -z-10 -rotate-[2.36deg] sm:right-16">
+          <span className="absolute bottom-32 left-[15%] sm:top-[clamp(21%,50%-15vw,40%)] w-[clamp(16rem,5rem+29.18vw,31.26rem)] -z-10 -rotate-[2.36deg] sm:right-[clamp(1.5rem,1rem+3.3vw,4rem)] sm:inset-auto">
             <img src={burger1Img} alt="burger" />
           </span>
         </div>
