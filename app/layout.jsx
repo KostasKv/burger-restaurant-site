e89cfcbd12f2e75c6f1b68e6efe719191f38170a
KostasKv <kostas.kvietinskas@gmail.com>
2023-08-19 14:02:34 +0100

    smooth scrolling

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${oswald.variable} ${bowlby.variable} font-serif bg-background`}>
         {children}
