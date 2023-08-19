f134498446398b97a53cfd05251f9f9b395b32ee
KostasKv <kostas.kvietinskas@gmail.com>
2023-08-19 17:28:11 +0100

    Responsive layout & matched to desktop design

--- app/page.jsx
+++ app/page.jsx
@@ -23,9 +23,9 @@ export default function Home() {
   return (
     <>
       <PrimaryHeader />
-      <main className="font-serif">
-        <section className="min-h-screen bg-primary flex justify-center">
-          <div className="relative w-full px-[clamp(1.25rem,6vw,5rem)] md:max-w-[1800px] isolate flex flex-col min-h-screen sm:px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem] sm:items-start">
+      <main className="font-serif w-full flex flex-col items-center">
+        <section className="min-h-screen w-full bg-primary flex justify-center">
+          <div className="relative w-full max-w-[1440px] px-[clamp(1.25rem,6vw,5rem)] isolate flex flex-col min-h-screen sm:px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem] sm:items-start sm:pb-16">
             <h1 className="relative font-serif-alt text-[clamp(1.5rem,1rem+6.4vw,6.875rem)] uppercase leading-none ">
               New York&apos;s <br /> favorite{" "}
               <span className="relative w-[1.08em] h-0 bg-purple-500 hidden sm:inline-block">
@@ -51,33 +51,39 @@ export default function Home() {
             </a>
           </div>
         </section>
-        <section id="about" className="relative px-5 py-16 sm:px-[3.12rem]">
+        <section
+          id="about"
+          className="relative px-5 py-16 max-w-[1440px] m-auto sm:px-[3.12rem] md:py-[12rem]">
           <h2 className="hidden">About</h2>
-          <p className="leading-none text-[clamp(2rem,6vw,4.5rem)] text-primary font-serif-alt uppercase max-w-[20ch]">
-            Hi, we&apos;re Jo&apos;s
-            <br />
-            Burgers, a collective of creators who
-            <br />
-            love burgers and good food.
-          </p>
-          <figure className="mt-8">
-            <Image
-              src={joKitchenImg}
-              alt="Jo working in the kitchen"
-              className="max-w-[min(100%,40rem)]"
-            />
-            <figcaption className="mt-3 text-xs text-center sm:text-lg md:text-2xl">
-              This is Jo. He started everything 40 years ago.
-            </figcaption>
-          </figure>
-          <div className="mt-16">
-            <Image
-              src={openUntilClosedImg}
-              alt='Sign saying "We are open until closed"'
-              className="max-w-[min(100%,40rem)]"
-            />
+          <div className="md:flex md:items-center">
+            <p className="leading-none text-[clamp(2rem,6vw,3.5rem)] text-primary font-serif-alt uppercase max-w-[20ch]">
+              Hi, we&apos;re Jo&apos;s
+              <br />
+              Burgers, a collective of creators who
+              <br />
+              love burgers and good food.
+            </p>
+            <figure className="mt-8 w-fit md:mt-0">
+              <Image
+                src={joKitchenImg}
+                alt="Jo working in the kitchen"
+                className="max-w-[min(100%,35rem)] mx-auto"
+              />
+              <figcaption className="mt-3 text-xs text-center sm:text-lg md:text-2xl">
+                This is Jo. He started everything 40 years ago.
+              </figcaption>
+            </figure>
+          </div>
+          <div className="mt-16 md:grid md:grid-cols-2 md:gap-20 lg:gap-40 md:items-center md:mt-48">
             <div>
-              <p className="mt-8 text-[2rem] leading-none text-primary font-serif-alt uppercase sm:text-5xl md:text-7xl">
+              <Image
+                src={openUntilClosedImg}
+                alt='Sign saying "We are open until closed"'
+                className="max-w-[min(100%,40rem)]"
+              />
+            </div>
+            <div>
+              <p className="mt-8 text-[2rem] leading-none text-primary font-serif-alt uppercase sm:text-5xl lg:text-7xl">
                 Food is for <br />
                 everyone.
               </p>
@@ -90,12 +96,13 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="relative">
+        <section className="w-full">
           <div className="min-h-[clamp(23.4375rem,10vw,37.5rem)] px-20 flex flex-col gap-4 justify-center items-center relative overflow-hidden md:flex-row">
             <Image
               src={videoPreviewImg}
               alt="background"
               className="absolute left-0 max-w-[100vw] grayscale -z-10"
+              placeholder="blur"
             />
             <div>
               <p className="leading-none uppercase text-white font-serif-alt text-2xl text-center sm:text-3xl md:text-[2.5rem]">
@@ -106,94 +113,107 @@ export default function Home() {
               <FontAwesomeIcon icon={faPlay} size="lg" />
             </button>
           </div>
-          <Image
-            src={organicMeatsIcon}
-            alt="Organic meats"
-            className="absolute bottom-0 left-[clamp(1.25rem,4vw,2.62rem)] translate-y-1/2 w-[clamp(7.52rem,2rem+10.16vw,13rem)]"
-          />
+          <div className="relative max-w-[1440px] m-auto">
+            <Image
+              src={organicMeatsIcon}
+              alt="Organic meats"
+              className="absolute bottom-0 left-[clamp(1.25rem,4vw,2.62rem)] translate-y-1/2 w-[clamp(7.52rem,2rem+10.16vw,13rem)]"
+            />
+          </div>
         </section>
-        <section id="menu" className="px-5 py-20 sm:px-[3.12rem]">
-          <h2 className="text-[2rem] leading-none text-primary font-serif-alt uppercase sm:text-5xl md:text-7xl">
-            Our Burgers
-          </h2>
-          <p className="mt-4 text-lg leading-normal text-primary sm:text-xl md:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <div className="mt-12 flex flex-col gap-10">
-            <div className="flex flex-col text-center bg-white">
-              <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
-                <Image
-                  src={burger1Img}
-                  alt="burger"
-                  placeholder="blur"
-                  className="absolute h-full w-full top-0 left-0 object-cover object-bottom"
-                />
+        <section
+          id="menu"
+          className="px-5 py-20 sm:px-[3.12rem] max-w-[1440px] m-auto md:py-40">
+          <div className="m-auto">
+            <h2 className="text-[2rem] leading-none text-primary font-serif-alt uppercase sm:text-5xl md:text-7xl">
+              Our Burgers
+            </h2>
+            <p className="mt-4 text-lg leading-normal text-primary sm:text-xl md:text-2xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="mt-12 m-auto max-w-xs grid grid-rows-4 grid-cols-1 gap-10 md:grid-rows-1 md:grid-cols-4 md:gap-4 md:max-w-none">
+              <div className="flex flex-col text-center bg-white">
+                <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
+                  <Image
+                    src={burger1Img}
+                    alt="burger"
+                    placeholder="blur"
+                    className="absolute h-full w-full top-0 left-0 object-cover object-bottom"
+                  />
+                </div>
+                <div className="p-6 w-full">
+                  <p className="uppercase font-serif-alt text-2xl">
+                    SHRIMP YO!
+                  </p>
+                  <p className="mt-1">
+                    Grilled shrimp burger with coleslaw in a homemade fluffy bun
+                  </p>
+                </div>
               </div>
-              <div className="p-6 w-full">
-                <p className="uppercase font-serif-alt text-2xl">SHRIMP YO!</p>
-                <p className="mt-1">
-                  Grilled shrimp burger with coleslaw in a homemade fluffy bun
-                </p>
+              <div className="flex flex-col text-center bg-white">
+                <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
+                  <Image
+                    src={burger2Img}
+                    alt="burger"
+                    placeholder="blur"
+                    className="absolute scale-[1.5] -translate-x-4 h-full w-full top-0 left-0 object-cover object-bottom"
+                  />
+                </div>
+                <div className="p-6 w-full">
+                  <p className="uppercase font-serif-alt text-2xl">
+                    DOUBLE TROUBLE
+                  </p>
+                  <p className="mt-1">
+                    Angus Beef burger with Gouda cheese and bacon in a sesame
+                    bun.
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col text-center bg-white">
-              <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
-                <Image
-                  src={burger2Img}
-                  alt="burger"
-                  placeholder="blur"
-                  className="absolute scale-[1.5] -translate-x-4 h-full w-full top-0 left-0 object-cover object-bottom"
-                />
+              <div className="flex flex-col text-center bg-white">
+                <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
+                  <Image
+                    src={burger3Img}
+                    alt="burger"
+                    placeholder="blur"
+                    className="absolute h-full w-full top-0 left-0 object-cover object-bottom"
+                  />
+                </div>
+                <div className="p-6 w-full">
+                  <p className="uppercase font-serif-alt text-2xl">JUICY JO</p>
+                  <p className="mt-1">
+                    Beef Burger with fried onion toped with ragu sauce in a rye
+                    bun.
+                  </p>
+                </div>
               </div>
-              <div className="p-6 w-full">
-                <p className="uppercase font-serif-alt text-2xl">
-                  DOUBLE TROUBLE
-                </p>
-                <p className="mt-1">
-                  Angus Beef burger with Gouda cheese and bacon in a sesame bun.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col text-center bg-white">
-              <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
-                <Image
-                  src={burger3Img}
-                  alt="burger"
-                  placeholder="blur"
-                  className="absolute h-full w-full top-0 left-0 object-cover object-bottom"
-                />
-              </div>
-              <div className="p-6 w-full">
-                <p className="uppercase font-serif-alt text-2xl">JUICY JO</p>
-                <p className="mt-1">
-                  Beef Burger with fried onion toped with ragu sauce in a rye
-                  bun.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col text-center bg-white">
-              <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
-                <Image
-                  src={burger4Img}
-                  alt="burger"
-                  placeholder="blur"
-                  className="absolute scale-[1.4] translate-x-4 translate-y-4 h-full w-full top-0 left-0 object-cover object-bottom"
-                />
-              </div>
-              <div className="p-6 w-full">
-                <p className="uppercase font-serif-alt text-2xl">CRISPY CHI</p>
-                <p className="mt-1">
-                  Beef Burger with fried onion toped with ragu sauce in a rye
-                  bun.
-                </p>
+              <div className="flex flex-col text-center bg-white">
+                <div className="relative w-full before:block before:pb-[100%] overflow-hidden">
+                  <Image
+                    src={burger4Img}
+                    alt="burger"
+                    placeholder="blur"
+                    className="absolute scale-[1.4] translate-x-4 translate-y-4 h-full w-full top-0 left-0 object-cover object-bottom"
+                  />
+                </div>
+                <div className="p-6 w-full">
+                  <p className="uppercase font-serif-alt text-2xl">
+                    CRISPY CHI
+                  </p>
+                  <p className="mt-1">
+                    Beef Burger with fried onion toped with ragu sauce in a rye
+                    bun.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </section>
-        <section id="review" className="px-5 pb-16 sm:px-[3.12rem]">
+        <section
+          id="review"
+          className="w-full max-w-[1440px] px-5 pb-16 sm:px-[3.12rem]">
           <h2 className="text-[2rem] leading-none text-primary font-serif-alt uppercase sm:text-5xl md:text-7xl">
             SOME REVIEWS
           </h2>
@@ -202,8 +222,25 @@ export default function Home() {
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
           {/* TODO: Create review card component & displayer component*/}
+          <div className="p-5 bg-white md:p-5 h-fit grid gap-x-4 grid-cols-[max-content,1fr] grid-rows-[max-content,1fr]">
+            <Image
+              src="https://i.pravatar.cc/48"
+              width={48}
+              height={48}
+              className="w-8 h-8 rounded-full md:w-12 md:h-12"
+            />
+            <span className="self-center text-sm font-semibold uppercase">
+              John Doe
+            </span>
+            <div></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          {/* </div> */}
         </section>
-        <section id="social">
+        <section id="social" className="w-full">
           <div className="w-full h-fit grid grid-cols-2 grid-rows-2 sm:grid-cols-5 sm:grid-rows-1">
             <div className="hidden relative before:pb-[100%] before:block sm:inline-block">
               <Image
@@ -240,36 +277,44 @@ export default function Home() {
           <h2 className="hidden">Social</h2>
         </section>
       </main>
-      <footer className="px-[1.25rem] pt-16 pb-12 bg-primary">
-        <span className={"text-3xl font-serif-alt cursor-pointer"}>
-          <Image src={companyLogo} alt="JO'S&copy;" />
-        </span>
-        <form className="flex flex-col">
-          <h2 className="mt-6 text-[2rem] leading-none font-serif-alt uppercase sm:text-4xl md:text-[2.5rem]">
-            Subscribe to our newsletter
-          </h2>
-          <p className="mt-4 text-sm sm:text-base">
-            By subscribing to our newsletter, you will receive the latest tips,
-            and promotions about our products and services straight to your
-            inbox.
-          </p>
-          <input
-            type="email"
-            name="newsletter"
-            id="newsletter-email"
-            placeholder="Enter your email"
-            className="mt-6 py-[1rem] px-1 text-center bg-transparent border-black border-2 placeholder:text-black"
-          />
-          <button
-            type="submit"
-            className="mt-4 py-3 text-xl font-semibold bg-black text-white uppercase sm:text-lg">
-            SUBSCRIBE
-          </button>
-        </form>
-        <div className="mt-12 mx-auto w-fit flex gap-3 uppercase text-sm sm:text-base">
-          <a className="cursor-pointer hover:underline">PRIVACY</a>
-          <a className="cursor-pointer hover:underline">TERMS</a>
-          <a className="cursor-pointer hover:underline">ACCESSIBILITY</a>
+      <footer className="flex justify-center px-[1.25rem] pt-16 pb-12 bg-primary sm:px-[3.12rem] md:pt-36">
+        <div className="max-w-[1440px] w-full">
+          <div className="max-w-[600px]">
+            <span className={"text-3xl font-serif-alt"}>
+              <Image
+                src={companyLogo}
+                alt="JO'S&copy;"
+                className="w-full max-w-[19.2rem]"
+              />
+            </span>
+            <h2 className="mt-6 text-[2rem] leading-none font-serif-alt uppercase sm:text-4xl md:text-[2.5rem] md:mt-10">
+              Subscribe to our newsletter
+            </h2>
+            <p className="mt-4 text-sm sm:text-base">
+              By subscribing to our newsletter, you will receive the latest
+              tips, and promotions about our products and services straight to
+              your inbox.
+            </p>
+            <form className="mt-6 flex flex-col md:flex-row md:mt-4 max-w-[34rem]">
+              <input
+                type="email"
+                name="newsletter"
+                id="newsletter-email"
+                placeholder="Enter your email"
+                className="flex-grow py-[1rem] px-1 text-center bg-transparent border-black border-2 placeholder:text-black"
+              />
+              <button
+                type="submit"
+                className="mt-4 py-3 px-6 text-xl font-semibold bg-black text-white uppercase sm:text-lg md:mt-0">
+                SUBSCRIBE
+              </button>
+            </form>
+            <div className="mt-12 mx-auto w-fit flex gap-3 uppercase text-sm sm:text-base md:mx-0 md:mt-20">
+              <a className="cursor-pointer hover:underline">PRIVACY</a>
+              <a className="cursor-pointer hover:underline">TERMS</a>
+              <a className="cursor-pointer hover:underline">ACCESSIBILITY</a>
+            </div>
+          </div>
         </div>
       </footer>
     </>
--- app/primary-header.jsx
+++ app/primary-header.jsx
@@ -13,35 +13,37 @@ export default function PrimaryHeader() {
   }
 
   return (
-    <header className="fixed flex justify-between items-center w-full px-[1.25rem] py-[1.06rem] isolate bg-primary z-10 sm:py-8 sm:px-[3.12rem]">
-      <span className={"text-3xl font-serif-alt cursor-pointer"}>
-        <Image src={companyLogo} alt="JO'S&copy;" />
-      </span>
-      <HamburgerMenuButton isExpanded={isMenuOpen} onClick={onMenuClick} />
-      <nav
-        className={`fixed -z-10 sm:relative duration-500 ${
-          isMenuOpen ? "visible translate-y-4" : "invisible -translate-y-full"
-        }  flex flex-col items-center justify-center bg-primary inset-0 sm:translate-y-0`}>
-        <ul
-          id="primary-navigation"
-          className={`flex flex-col gap-8 text-2xl font-semibold leading-normal uppercase sm:flex-row sm:visible sm:static`}>
-          <li className="hover:text-accent duration-75">
-            <a href="#about">About</a>
-          </li>
-          <li className="hover:text-accent duration-75">
-            <a href="#menu">Menu</a>
-          </li>
-          <li className="hover:text-accent duration-75">
-            <a href="#review">Review</a>
-          </li>
-          <li className="hover:text-accent duration-75">
-            <a href="#social">Social</a>
-          </li>
-        </ul>
-        <span className="hsm: sm:hidden mt-[min(4rem,5vh)]">
-          <Image src={smileIcon} alt="" />
+    <header className="fixed w-full px-[1.25rem] py-[1.06rem] isolate bg-primary z-10 sm:py-8 sm:px-[3.12rem]">
+      <div className="max-w-[1440px] m-auto flex justify-between items-center">
+        <span className={"text-3xl font-serif-alt cursor-pointer"}>
+          <Image src={companyLogo} alt="JO'S&copy;" />
         </span>
-      </nav>
+        <HamburgerMenuButton isExpanded={isMenuOpen} onClick={onMenuClick} />
+        <nav
+          className={`fixed -z-10 sm:relative duration-500 ${
+            isMenuOpen ? "visible translate-y-4" : "invisible -translate-y-full"
+          }  flex flex-col items-center justify-center bg-primary inset-0 sm:translate-y-0`}>
+          <ul
+            id="primary-navigation"
+            className={`flex flex-col gap-8 text-2xl font-semibold leading-normal uppercase sm:flex-row sm:visible sm:static`}>
+            <li className="hover:text-accent duration-75">
+              <a href="#about">About</a>
+            </li>
+            <li className="hover:text-accent duration-75">
+              <a href="#menu">Menu</a>
+            </li>
+            <li className="hover:text-accent duration-75">
+              <a href="#review">Review</a>
+            </li>
+            <li className="hover:text-accent duration-75">
+              <a href="#social">Social</a>
+            </li>
+          </ul>
+          <span className="hsm: sm:hidden mt-[min(4rem,5vh)]">
+            <Image src={smileIcon} alt="" />
+          </span>
+        </nav>
+      </div>
     </header>
   );
 }
