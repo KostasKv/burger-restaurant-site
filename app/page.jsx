a0faa52e82467a46f04dbd24e5877ad52c3c99fe
KostasKv <kostas.kvietinskas@gmail.com>
2023-08-23 18:40:49 +0100

    Improved first section layout & responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -24,29 +24,27 @@ export default function Home() {
     <>
       <PrimaryHeader />
       <main className="font-serif w-full flex flex-col items-center">
-        <section className="min-h-screen w-full bg-primary flex justify-center max-h-[1200px]">
-          <div className="relative w-full max-w-[1440px] px-[clamp(1.25rem,6vw,5rem)] isolate flex flex-col  sm:px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem] sm:items-start sm:pb-16">
-            <h1 className="relative font-serif-alt text-[clamp(1.5rem,1rem+6.4vw,6.875rem)] uppercase leading-none ">
-              New York&apos;s <br /> favorite{" "}
-              <span className="relative w-[1.08em] h-0 bg-purple-500 hidden sm:inline-block">
-                <Image
-                  src={smileIcon}
-                  alt=""
-                  className="absolute -left-[0.3em] -translate-y-[84%]"
-                />
-              </span>
-              <br /> burger
-            </h1>
-            <p className="mt-3 text-lg sm:text-2xl max-w-[clamp(24rem,10rem+45vw,38rem)] sm:mt-[1.19rem] bg-inherit bg-clip-text">
-              In a world so painfully serious, yet so ridiculous, you knew it
-              was only a matter of time before plant-based steak became a thing.
-            </p>
-            <a
-              href="#menu"
-              className="flex justify-center items-center mt-auto w-full h-[3.375rem] max-w-[20rem] text-xl bg-black text-white uppercase sm:max-w-[16.875rem] sm:text-2xl sm:mt-12 hover:bg-accent focus:bg-accent duration-75 hover:text-black focus:text-black focus:outline-none">
-              Browse Menu
-            </a>
-            <span className="absolute bottom-32 left-[15%] sm:top-[clamp(24%,50%-15vw,40%)] w-[clamp(16rem,5rem+29.18vw,31.26rem)] -z-10 -rotate-[2.36deg] sm:right-[clamp(1.5rem,1rem+3.3vw,4rem)] sm:inset-auto">
+        <section className="min-h-[min(100vh,1000px)] w-full bg-primary flex justify-center max-h-[1200px]">
+          <div className="relative items-center justify-between h-fit gap-6 w-full max-w-[1440px] px-[clamp(1.25rem,6vw,5rem)] isolate flex flex-col sm:px-[clamp(1.25rem,0.5rem+2.64vw,3rem)] sm:pt-[11.25rem] pb-10 pt-[6.69rem] sm:items-start sm:pb-16">
+            <div>
+              <h1 className="relative font-serif-alt text-[clamp(1.5rem,1rem+6.4vw,6.875rem)] uppercase leading-none ">
+                New York&apos;s <br /> favorite{" "}
+                <span className="relative w-[1.08em] h-0 bg-purple-500 hidden sm:inline-block">
+                  <Image
+                    src={smileIcon}
+                    alt=""
+                    className="absolute -left-[0.3em] -translate-y-[84%]"
+                  />
+                </span>
+                <br /> burger
+              </h1>
+              <p className="mt-3 text-lg sm:text-2xl max-w-[clamp(24rem,10rem+45vw,38rem)] sm:mt-[1.19rem] bg-inherit bg-clip-text">
+                In a world so painfully serious, yet so ridiculous, you knew it
+                was only a matter of time before plant-based steak became a
+                thing.
+              </p>
+            </div>
+            <span className="w-[70%] sm:absolute sm:top-[clamp(24%,50%-15vw,40%)] sm:w-[clamp(16rem,5rem+29.18vw,31.26rem)] -z-10 -rotate-[2.36deg] sm:right-[clamp(1.5rem,1rem+3.3vw,2rem)] sm:inset-auto">
               <Image
                 src={burger1Img}
                 alt="burger"
@@ -54,6 +52,11 @@ export default function Home() {
                 placeholder="blur"
               />
             </span>
+            <a
+              href="#menu"
+              className="flex justify-center items-center mt-4 w-full h-[3.375rem] max-w-[20rem] text-xl bg-black text-white uppercase sm:max-w-[16.875rem] sm:text-2xl sm:mt-12 hover:bg-accent focus:bg-accent duration-75 hover:text-black focus:text-black focus:outline-none">
+              Browse Menu
+            </a>
           </div>
         </section>
         <section
